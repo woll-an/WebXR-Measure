6e9610f698bd227f9e96eb53a22a240ee44d1178
Annika Wollschläger <annika.wollschlaeger@tum.de>
2020-07-06 11:53:21 +0200

    draw lines during measure

--- src/xr-session.js
+++ src/xr-session.js
@@ -2,22 +2,23 @@ import * as THREE from 'three';
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
-var container;
-var camera, scene, renderer;
-var controller;
+let container;
+let camera, scene, renderer;
+let controller;
 
-var reticle;
-
-var hitTestSource = null;
-var hitTestSourceRequested = false;
+let hitTestSource = null;
+let hitTestSourceRequested = false;
 
 let measurements = [];
 
+let reticle;
+let currentLine = null;
+
 function makeTextSprite( text )
 {
-  var size = 20
-	var canvas = document.createElement("canvas");
-  var context = canvas.getContext("2d");
+  const size = 20
+	let canvas = document.createElement("canvas");
+  let context = canvas.getContext("2d");
 
   context.strokeStyle = "white";
   context.font = size + "pt Arial";
@@ -28,12 +29,12 @@ function makeTextSprite( text )
   context.fillText(text, canvas.width / 2, canvas.height / 2);
 	
 	// canvas contents will be used for a texture
-  var texture = new THREE.Texture(canvas)
+  let texture = new THREE.Texture(canvas)
   texture.needsUpdate = true;
-  var spriteMaterial = new THREE.SpriteMaterial({ map: texture} );
+  let spriteMaterial = new THREE.SpriteMaterial({ map: texture} );
   spriteMaterial.depthWrite = false;
   spriteMaterial.depthTest = false;
-	var sprite = new THREE.Sprite(spriteMaterial);
+	let sprite = new THREE.Sprite(spriteMaterial);
 	sprite.scale.set(0.25,0.25,1.0);
 	return sprite;	
 }
@@ -44,20 +45,29 @@ function matrixToVector(matrix) {
   return vector;
 }
 
-function getLine(points) {
-  var lineMaterial = new THREE.LineBasicMaterial({
+function initLine(point) {
+  let lineMaterial = new THREE.LineBasicMaterial({
     color: 0xffffff,
     linewidth: 5,
     linecap: 'round'
   });
 
-  var lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+  let lineGeometry = new THREE.BufferGeometry().setFromPoints([point, point]);
   return new THREE.Line(lineGeometry, lineMaterial);
 }
 
+function updateLine(matrix) {
+  let positions = currentLine.geometry.attributes.position.array;
+  positions[3] = matrix.elements[12]
+  positions[4] = matrix.elements[13]
+  positions[5] = matrix.elements[14]
+  currentLine.geometry.attributes.position.needsUpdate = true;
+  currentLine.geometry.computeBoundingSphere();
+}
+
 function initReticle() {
-  let ring = new THREE.RingBufferGeometry(0.07, 0.1, 32).rotateX(- Math.PI / 2);
-  let dot = new THREE.CircleBufferGeometry(0.01, 32).rotateX(- Math.PI / 2);
+  let ring = new THREE.RingBufferGeometry(0.045, 0.05, 32).rotateX(- Math.PI / 2);
+  let dot = new THREE.CircleBufferGeometry(0.005, 32).rotateX(- Math.PI / 2);
   reticle = new THREE.Mesh(
     BufferGeometryUtils.mergeBufferGeometries([ring, dot]),
     new THREE.MeshBasicMaterial()
@@ -73,14 +83,6 @@ function initRenderer() {
   renderer.xr.enabled = true;
 }
 
-function getMarkerFromSelection(matrix) {
-  var circleGeometry = new THREE.CircleBufferGeometry(0.01, 32).rotateX(- Math.PI / 2);
-  var material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
-  var mesh = new THREE.Mesh(circleGeometry, material);
-  mesh.position.setFromMatrixPosition(matrix);
-  return mesh
-}
-
 function getDistance(points) {
   if (points.length == 2)
     return points[0].distanceTo(points[1]);
@@ -91,7 +93,7 @@ function initXR() {
   document.body.appendChild(container);
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-  var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  let light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
   light.position.set(0.5, 1, 0.25);
   scene.add(light);
   initRenderer()
@@ -110,14 +112,16 @@ function onSelect() {
   if (reticle.visible) {
     measurements.push(matrixToVector(reticle.matrix));
     if (measurements.length == 2) {
-      console.log(getDistance(measurements));
-      scene.add(getLine(measurements));
-      var sprite = makeTextSprite(Math.round(getDistance(measurements)*100) + ' cm');
+      let distance = Math.round(getDistance(measurements)*100);
+      let sprite = makeTextSprite(distance + ' cm');
       sprite.position.setFromMatrixPosition(reticle.matrix);
       scene.add(sprite);
       measurements = [];
+      currentLine = null;
+    } else {
+      currentLine = initLine(measurements[0]);
+      scene.add(currentLine);
     }
-    scene.add(getMarkerFromSelection(reticle.matrix));
   }
 }
 
@@ -133,8 +137,8 @@ function animate() {
 
 function render(timestamp, frame) {
   if (frame) {
-    var referenceSpace = renderer.xr.getReferenceSpace();
-    var session = renderer.xr.getSession();
+    let referenceSpace = renderer.xr.getReferenceSpace();
+    let session = renderer.xr.getSession();
     if (hitTestSourceRequested === false) {
       session.requestReferenceSpace('viewer').then(function (referenceSpace) {
         session.requestHitTestSource({ space: referenceSpace }).then(function (source) {
@@ -149,13 +153,17 @@ function render(timestamp, frame) {
     }
 
     if (hitTestSource) {
-      var hitTestResults = frame.getHitTestResults(hitTestSource);
+      let hitTestResults = frame.getHitTestResults(hitTestSource);
       if (hitTestResults.length) {
-        var hit = hitTestResults[0];
+        let hit = hitTestResults[0];
         reticle.visible = true;
         reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
       } else {
         reticle.visible = false;
+      }
+
+      if (currentLine) {
+        updateLine(reticle.matrix);
       }
     }
   }
